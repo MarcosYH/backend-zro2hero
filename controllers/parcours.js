2307fda424c1d874fe2b01718f37f825ff701f82
MarcosYH <marcosdeimos18@gmail.com>
2023-10-08 12:13:41 +0200

    feat: change promise for createPath

--- controllers/parcours.js
+++ controllers/parcours.js
@@ -8,25 +8,26 @@ const dotenv = require("dotenv");
 dotenv.config();
 app.use(cookieParser());
 
-exports.createParcours = async (request, response) => {
-  try {
-    const { wording, description, image, categorie } = request.body;
-    const nouveauParcours = new Parcours({
-      wording,
-      description,
-      image,
-      categorie,
-    });
-    await nouveauParcours.save();
-    response
-      .status(201)
-      .json({
+exports.createParcours = (request, response) => {
+  const { wording, description, image, categorie } = request.body;
+  const nouveauParcours = new Parcours({
+    wording,
+    description,
+    image,
+    categorie,
+  });
+
+  nouveauParcours
+    .save()
+    .then((parcours) => {
+      response.status(201).json({
         message: "Parcours créé avec succès",
-        parcours: nouveauParcours,
+        parcours: parcours,
       });
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
+    })
+    .catch((error) => {
+      response.status(500).json({ error: error.message });
+    });
 };
 
 exports.getAllParcours = async (request, response) => {
@@ -39,39 +40,40 @@ exports.getAllParcours = async (request, response) => {
 };
 
 exports.updateParcours = async (request, response) => {
-    const { id } = request.params;
-    const { wording, description, image, categorie } = request.body;
-  
-    try {
-      const parcours = await Parcours.findByIdAndUpdate(
-        id,
-        { wording, description, image, categorie },
-        { new: true } 
-      );
-  
-      if (!parcours) {
-        return response.status(404).json({ error: "Parcours non trouvé" });
-      }
-  
-      response.status(200).json({ message: "Parcours mis à jour avec succès", parcours });
-    } catch (error) {
-      response.status(500).json({ error: error.message });
+  const { id } = request.params;
+  const { wording, description, image, categorie } = request.body;
+
+  try {
+    const parcours = await Parcours.findByIdAndUpdate(
+      id,
+      { wording, description, image, categorie },
+      { new: true }
+    );
+
+    if (!parcours) {
+      return response.status(404).json({ error: "Parcours non trouvé" });
     }
+
+    response
+      .status(200)
+      .json({ message: "Parcours mis à jour avec succès", parcours });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
   }
-  
-  exports.deleteParcours = async (request, response) => {
-    const { id } = request.params; // Récupérez l'ID du parcours à supprimer depuis les paramètres de l'URL
-  
-    try {
-      const parcours = await Parcours.findByIdAndDelete(id);
-  
-      if (!parcours) {
-        return response.status(404).json({ error: "Parcours non trouvé" });
-      }
-  
-      response.status(200).json({ message: "Parcours supprimé avec succès" });
-    } catch (error) {
-      response.status(500).json({ error: error.message });
+};
+
+exports.deleteParcours = async (request, response) => {
+  const { id } = request.params; // Récupérez l'ID du parcours à supprimer depuis les paramètres de l'URL
+
+  try {
+    const parcours = await Parcours.findByIdAndDelete(id);
+
+    if (!parcours) {
+      return response.status(404).json({ error: "Parcours non trouvé" });
     }
+
+    response.status(200).json({ message: "Parcours supprimé avec succès" });
+  } catch (error) {
+    response.status(500).json({ error: error.message });
   }
-  
+};
