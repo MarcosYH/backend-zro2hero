a4699356534075c487a39b95d300d505d10157c2
MarcosYH <marcosdeimos18@gmail.com>
2024-01-06 17:27:29 +0100

    feat: function createCourse

--- routes/courses.js
+++ routes/courses.js
@@ -7,7 +7,7 @@ const courseController = require("../controllers/courses")
 
 router.use(cookieParser());
 
-router.post("/create", courseController.createCourses);
+router.post("/create", courseController.createCourse);
 
 
 module.exports = router;
