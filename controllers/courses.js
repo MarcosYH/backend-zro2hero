072b9e8c2542a366fe48ae2f107b40c92f2a47e8
MarcosYH <marcosdeimos18@gmail.com>
2024-01-06 17:28:45 +0100

    feat: controller for course

---
+++ controllers/courses.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const Course = require("../db/courseModel");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+app.use(cookieParser());
+
+//function not completely
+exports.createCourse = async (request, response) => {
+    try {
+        const { title, description, duration } = request.body;
+
+        // Create a new course using the Course model
+        const newCourse = new Course({
+            title,
+            description,
+            duration,
+        });
+
+        // Save the new course to the database
+        const savedCourse = await newCourse.save();
+
+        response.status(201).json(savedCourse);
+    } catch (error) {
+        response.status(500).json({ error: "Failed to create course" });
+    }
+};
